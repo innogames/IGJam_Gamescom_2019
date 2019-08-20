731f231abed047e2590d35359429dcc3b5045d41
Friedrich Wessel <friedrich.wessel@innogames.com>
2019-08-20 16:07:17 +0200

    add third button to inputManager

--- igjam/Assets/Scripts/Ship/Ship.cs
+++ igjam/Assets/Scripts/Ship/Ship.cs
@@ -1,53 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
-public class Ship : MonoBehaviour {
+public class Ship : MonoBehaviour
+{
+	[HideInInspector()] public Rigidbody2D body;
 
-    [HideInInspector ()]
-    public Rigidbody2D body;
+	[HideInInspector()] public List<PartFixture> partFixtures = new List<PartFixture>();
 
-    [HideInInspector ()]
-    public List<PartFixture> partFixtures = new List<PartFixture> ();
+	// already to the ship
+	public List<ShipPart> parts = new List<ShipPart>();
+	private SignalBus _signalBus;
 
-    // already to the ship
-    public List<ShipPart> parts = new List<ShipPart> ();
+	[Inject]
+	void Init(SignalBus signalBus)
+	{
+		body = GetComponent<Rigidbody2D>();
+		partFixtures.Clear();
+		foreach (PartFixture f in GetComponentsInChildren<PartFixture>())
+		{
+			partFixtures.Add(f);
+		}
 
-    void Awake () {
-        body = GetComponent<Rigidbody2D> ();
-        partFixtures.Clear ();
-        foreach (PartFixture f in GetComponentsInChildren<PartFixture> ()) {
-            partFixtures.Add (f);
-        }
-    }
+		_signalBus = signalBus;
+		_signalBus.Subscribe<SystemSignal.Ship.PartAttached>(AttachNewPart);
+	}
 
-    void Update () {
-        HandleParts ();
+	private void AttachNewPart(SystemSignal.Ship.PartAttached signal)
+	{
+		AddControl(signal.NewPart, signal.SlotId);
+	}
 
-    }
+	void Update()
+	{
+		HandleParts();
+	}
 
-    public void AddControl (ShipPart part, int slotId) {
+	public void AddControl(ShipPart part, int slotId)
+	{
+		// part.body = body;
+		// parts.Add (part);
+		// parts[slotId].add(part); 
 
-        // part.body = body;
-        // parts.Add (part);
-        // parts[slotId].add(part); 
+		partFixtures[slotId].part = part;
+	}
 
-        partFixtures[slotId].part = part;
-    }
+	public void SelectNextControl()
+	{
+	}
 
-    public void SelectNextControl () { }
-
-    void HandleParts () {
-        // roll thtorugh list and update their position and rotation and such 
-        int i = 0;
-        foreach (ShipPart part in parts) {
-
-            // if (currentpart == i) {
-            //     part.Activate ();
-            // } else part.Deactivate ();
-            // part.TalkWithShip (this);
-            // i++;
-        }
-
-    }
+	void HandleParts()
+	{
+		// roll thtorugh list and update their position and rotation and such 
+		int i = 0;
+		foreach (ShipPart part in parts)
+		{
+			// if (currentpart == i) {
+			//     part.Activate ();
+			// } else part.Deactivate ();
+			// part.TalkWithShip (this);
+			// i++;
+		}
+	}
 }
