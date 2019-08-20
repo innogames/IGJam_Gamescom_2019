1be9d3db84021246308bbb57af4cb4e9d59fc497
Friedrich Wessel <friedrich.wessel@innogames.com>
2019-08-20 13:37:51 +0200

    basic selection UI

---
+++ igjam/Assets/Scripts/UI/ShipSlotUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Button))]
+public class ShipSlotUI :  SelectableControl
+{
+	private Button _connectedButton;
+
+	void Start()
+	{
+		_connectedButton = GetComponent<Button>();
+	}
+
+	public override void Select()
+	{
+		_connectedButton.image.color = Color.red;
+	}
+
+	public override void Deselect()
+	{
+		_connectedButton.image.color = Color.white;
+	}
+
+	public override void Activate()
+	{
+		_connectedButton.image.color = Color.green;
+	}
+}
