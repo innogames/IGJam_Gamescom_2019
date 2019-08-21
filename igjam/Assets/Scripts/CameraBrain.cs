e467ec2d81cdaf4c54ad5788dec4f381be5da183
dittomat <dittomakesgames@gmail.com>
2019-08-21 13:33:46 +0200

    camera

---
+++ igjam/Assets/Scripts/CameraBrain.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class CameraBrain : MonoBehaviour {
+
+    public float buildzoomlevel = 10;
+    public float gamezoomlevel = 35;
+
+    // i'm sorry for creating static variables. 
+    // i just couldn't deal 
+    public static bool zoomedIn = false;
+    public static Vector2 shipposition;
+
+    Vector2 startpos;
+
+    Camera cam;
+
+    SignalBus _signalBus;
+
+    [Inject]
+    void Init (SignalBus signalBus) {
+        cam = Camera.main;
+        startpos = transform.position;
+        _signalBus = signalBus;
+        _signalBus.Subscribe<SystemSignal.GameMode.ConfigureShip.Activate> (ActivateUI);
+        _signalBus.Subscribe<SystemSignal.GameMode.ConfigureControls.Activate> (ActivateUI);
+        _signalBus.Subscribe<SystemSignal.GameMode.ConfigureShip.Deactivate> (DeactivateUI);
+        _signalBus.Subscribe<SystemSignal.GameMode.ConfigureControls.Deactivate> (DeactivateUI);
+
+    }
+
+    void DeactivateUI () { zoomedIn = false; }
+    void ActivateUI () { zoomedIn = true; shipposition = FindObjectOfType<Ship> ().transform.position; }
+
+    void Update () {
+
+        Vector2 targetpos = startpos;
+        float targetzoom = gamezoomlevel;
+
+        if (zoomedIn) {
+            targetzoom = buildzoomlevel;
+            targetpos = shipposition;
+        }
+
+        cam.orthographicSize = Mathf.Lerp (cam.orthographicSize, targetzoom, .1f);
+
+        transform.position = Vector3.Lerp (transform.position, new Vector3 (targetpos.x, targetpos.y, -10f), .1f);
+    }
+}
--- igjam/Assets/Scripts/UI/ControllerConfigurationView.cs
+++ igjam/Assets/Scripts/UI/ControllerConfigurationView.cs
@@ -4,82 +4,70 @@ using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
 
-public class ControllerConfigurationView : MonoBehaviour
-{
-	private enum ConfigurationState
-	{
-		SelectControlSlot, 
-		SelectButton
-	}
+public class ControllerConfigurationView : MonoBehaviour {
+    private enum ConfigurationState {
+        SelectControlSlot,
+        SelectButton
+    }
 
-	public ListUI ShipControls;
+    public ListUI ShipControls;
 
-	private ConfigurationState _state;
-	private SignalBus _signalBus;
+    private ConfigurationState _state;
+    private SignalBus _signalBus;
 
-	[Inject]
-	void Init(SignalBus signalBus)
-	{
-		_signalBus = signalBus;
-		_signalBus.Subscribe<SystemSignal.GameMode.ConfigureControls.Activate>(ActivateUI);
-		_signalBus.Subscribe<SystemSignal.GameMode.ConfigureControls.Deactivate>(DeactivateUI);
-		ActivateShipSlots();
-		DeactivateUI();
-	}
+    [Inject]
+    void Init (SignalBus signalBus) {
+        _signalBus = signalBus;
+        _signalBus.Subscribe<SystemSignal.GameMode.ConfigureControls.Activate> (ActivateUI);
+        _signalBus.Subscribe<SystemSignal.GameMode.ConfigureControls.Deactivate> (DeactivateUI);
+        ActivateShipSlots ();
+        DeactivateUI ();
+    }
 
-	private void DeactivateUI()
-	{
-		this.enabled = false;
-	}
+    private void DeactivateUI () {
+        this.enabled = false;
+    }
 
-	private void ActivateUI()
-	{
-		this.enabled = true;
-		ActivateShipSlots();
-	}
+    private void ActivateUI () {
+        this.enabled = true;
+        ActivateShipSlots ();
+    }
 
-	void Update()
-	{
-		if (Input.GetButtonUp("A"))
-		{
-			switch (_state)
-			{
-				case ConfigurationState.SelectControlSlot:
-					ShipControls.SelectNextSlot();
-					break; 
-				case ConfigurationState.SelectButton:
-					_signalBus.Fire(new SystemSignal.Ship.ControlUpdated(ShipControls.CurrentSlotId, "A"));
-					ActivateShipSlots();
-					break;
-			}
-			
-		}
-		if (Input.GetButtonUp("B"))
-		{
-			ShipControls.ActivateSelectedSlot();
-			switch (_state)
-			{
-				case ConfigurationState.SelectControlSlot:
-					ActivateControlSelection();
-					break; 
-				case ConfigurationState.SelectButton:
-					_signalBus.Fire(new SystemSignal.Ship.ControlUpdated(ShipControls.CurrentSlotId, "B"));
-					ActivateShipSlots();
-					break;
-			}
+    void Update () {
+        if (Input.GetButtonUp ("A")) {
+            switch (_state) {
+                case ConfigurationState.SelectControlSlot:
+                    ShipControls.SelectNextSlot ();
+                    break;
+                case ConfigurationState.SelectButton:
+                    _signalBus.Fire (new SystemSignal.Ship.ControlUpdated (ShipControls.CurrentSlotId, "A"));
+                    ActivateShipSlots ();
+                    break;
+            }
 
-		}
-	}
+        }
+        if (Input.GetButtonUp ("B")) {
+            ShipControls.ActivateSelectedSlot ();
+            switch (_state) {
+                case ConfigurationState.SelectControlSlot:
+                    ActivateControlSelection ();
+                    break;
+                case ConfigurationState.SelectButton:
+                    _signalBus.Fire (new SystemSignal.Ship.ControlUpdated (ShipControls.CurrentSlotId, "B"));
+                    ActivateShipSlots ();
+                    break;
+            }
 
-	private void ActivateControlSelection()
-	{
-		ShipControls.Deactivate();
-		_state = ConfigurationState.SelectButton;
-	}
+        }
+    }
 
-	private void ActivateShipSlots()
-	{
-		_state = ConfigurationState.SelectControlSlot;
-		ShipControls.Activate();
-	}
+    private void ActivateControlSelection () {
+        ShipControls.Deactivate ();
+        _state = ConfigurationState.SelectButton;
+    }
+
+    private void ActivateShipSlots () {
+        _state = ConfigurationState.SelectControlSlot;
+        ShipControls.Activate ();
+    }
 }
