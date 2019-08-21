014c3daf58bc3b4c9fa54200c15eadea8fc7c8c0
Friedrich Wessel <friedrich.wessel@innogames.com>
2019-08-22 00:39:46 +0200

    Generated Aliens react on color

---
+++ igjam/Assets/Scripts/FaceGenerator/ActiveOnShipPartButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveOnShipPartButton : MonoBehaviour
+{
+	public string Button = "A";
+	private ShipPart _internalConnectedPart;
+
+	private ShipPart ConnectedPart
+	{
+		get
+		{
+			if (_internalConnectedPart == null)
+			{
+				_internalConnectedPart = GetComponentInParent<ShipPart>();
+			}
+
+			return _internalConnectedPart;
+		}
+	}
+
+	private void OnEnable()
+	{
+		if (ConnectedPart != null)
+		{
+			gameObject.SetActive(Button == ConnectedPart.button);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+}
