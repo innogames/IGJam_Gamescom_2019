600cad8d4c1cf95636bd040c0fab45f2d31274c3
dittomat <dittomakesgames@gmail.com>
2019-08-21 14:11:03 +0200

    shielder setup
    doesn't work yet

---
+++ igjam/Assets/Scripts/Aliens/Shielder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shielder : ShipPart {
+    
+    SFXLoop sfx;
+    private SpriteRenderer _shownView;
+
+    public Sprite PinkShielder;
+    public Sprite BlueShielder;
+
+    void Awake () {
+        sfx = GetComponent<SFXLoop> ();
+        _shownView = GetComponent<SpriteRenderer> ();
+    }
+
+    public override void Draw () { }
+
+    public override void Activate () {
+        base.Activate ();
+        sfx.Play ();
+        // GROW !! 
+    }
+
+    public override void AssignButton (string newControlName) {
+        base.AssignButton (newControlName);
+        if (button == "A") {
+            _shownView.sprite = BlueShielder;
+        } else {
+            _shownView.sprite = PinkShielder;
+        }
+    }
+}
