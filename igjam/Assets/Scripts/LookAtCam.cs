1a1a9cb0728b1599a95026fc2ce83d9dbdd0db76
Larsss <blogthug@gmail.com>
2019-08-21 13:10:36 +0200

    Head pieces

---
+++ igjam/Assets/Scripts/LookAtCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCam : MonoBehaviour
+{
+
+    //public Vector3 offset;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.eulerAngles = new Vector3(0, 180, 0);
+        //this.transform.LookAt(target);
+    }
+}
