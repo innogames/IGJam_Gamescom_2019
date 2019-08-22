0688d64392b23a00aa1addaff1f4ec586ddac8bf
dittomat <dittomakesgames@gmail.com>
2019-08-22 10:11:33 +0200

    voice system
    voice system implemented, only thruster is playing tho,
    
    COOL COOL COOL :D::D:D

---
+++ igjam/Assets/Scripts/Aliens/AlienVoice.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienVoice : MonoBehaviour {
+
+    public List<AudioClip> hoverOver = new List<AudioClip> ();
+    public List<AudioClip> pickUp = new List<AudioClip> ();
+    public List<AudioClip> popOut = new List<AudioClip> ();
+
+    public List<AudioClip> assign = new List<AudioClip> ();
+    public List<AudioClip> shield = new List<AudioClip> ();
+    public List<AudioClip> thruster = new List<AudioClip> ();
+
+    AudioSource src;
+
+    void Start () {
+        cooldown = .1f;
+
+        GameObject g = new GameObject ("voice");
+        g.transform.SetParent (transform);
+        g.transform.localPosition = Vector2.zero;
+        src = g.AddComponent<AudioSource> ();
+
+    }
+
+    float cooldown;
+
+    void Update () {
+        if (cooldown > 0) {
+            cooldown -= Time.deltaTime;
+        }
+    }
+
+    // takes a random clip from l and plays it 
+    public void Say (List<AudioClip> l) {
+
+        if (cooldown > 0) return;
+
+        int r = Random.Range (0, l.Count);
+        src.clip = l[r];
+        src.pitch = 1f + Random.Range (-1f, 1f) * .05f;
+        src.Play ();
+        cooldown = src.clip.length + Random.Range (.5f, 1.5f);
+    }
+}
---
+++ igjam/Assets/Scripts/Aliens/VoiceContainer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceContainer : MonoBehaviour {
+
+    public List<AlienVoice> voices = new List<AlienVoice> ();
+
+    static VoiceContainer inst;
+
+    int counter;
+
+    void Awake () {
+        inst = this;
+        int n = voices.Count;
+        // scramble
+        for (int i = 0; i < n; i++) {
+            int r = Random.Range (i, n);
+            AlienVoice av = voices[r];
+            voices[r] = voices[i];
+            voices[i] = av;
+        }
+    }
+
+    public static AlienVoice GetVoiceSetup () {
+        int i = inst.counter;
+        inst.counter++;
+        return (Instantiate (inst.voices[i], Vector2.zero, Quaternion.identity).GetComponent<AlienVoice> ());
+    }
+
+}
